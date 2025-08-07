f3bcb6737b96bb13151cd3cde533633eff4c88fb
Hesham243 <heshamahmed243@gmail.com>
2025-08-07 09:26:25 +0300

    init commit

---
+++ controllers/users.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const SALT_LENGTH = 12
+
+router.post('/signup', async (req, res) => {
+  try {
+    // Check if the username is already taken
+    const userInDatabase = await User.findOne({ username: req.body.username })
+    if (userInDatabase) {
+      return res.json({ error: 'Username already taken.' })
+    }
+
+    // Create a new user with hashed password
+    const user = await User.create({
+      username: req.body.username,
+      hashedPassword: bcrypt.hashSync(req.body.password, SALT_LENGTH),
+    })
+
+    // Create the token, add user credentials like name, the created _id from MongoDB
+    const token = jwt.sign(
+      { 
+        username: user.username, 
+        _id: user._id 
+      },
+      process.env.JWT_SECRET
+    )
+
+    res.status(201).json({ user, token })
+    
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+
+
+router.post('/signin', async (req, res) => {
+  try {
+    // Find the user from MongoDB
+    const user = await User.findOne({ username: req.body.username })
+    
+    // If user exist and the received hashedPass same as in MongoDB = true else Invalid name or pass
+    if (user && bcrypt.compareSync(req.body.password, user.hashedPassword)) {
+      const token = jwt.sign(
+        { 
+          username: user.username, 
+          _id: user._id 
+        },
+        process.env.JWT_SECRET
+      )
+      res.status(200).json({ token })
+
+    } else {
+      res.status(401).json({ error: 'Invalid username or password.' })
+    }
+
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+module.exports = router
