41d64044a79b4d797ece3624d9be079c8c316087
Hesham243 <heshamahmed243@gmail.com>
2025-08-07 10:23:47 +0300

    create route done

--- server.js
+++ server.js
@@ -3,17 +3,14 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const testJWTRouter = require('./controllers/test-jwt')
-const usersRouter = require('./controllers/users')
-const profilesRouter = require('./controllers/profiles')
 const cors = require('cors')
 
 
-mongoose.connect(process.env.MONGODB_URI)
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
-})
+// Controllers
+const profilesRouter = require('./controllers/profiles')
+const testJWTRouter = require('./controllers/test-jwt')
+const usersRouter = require('./controllers/users')
+const hootsRouter = require('./controllers/hoots.js')
 
 
 //MIDDLEWARE
@@ -22,10 +19,19 @@ app.use(cors())
 
 
 // Routes go here
-app.use('/test-jwt', testJWTRouter)
 app.use('/users', usersRouter)
-app.use('/profiles', profilesRouter)
+app.use('/hoots', hootsRouter)
 
+
+// app.use('/test-jwt', testJWTRouter)
+// app.use('/profiles', profilesRouter)
+
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
 
 app.listen(3000, () => {
   console.log('The express app is ready!')
